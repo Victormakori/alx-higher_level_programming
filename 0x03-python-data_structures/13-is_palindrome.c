3fe171a008f77f1d1e1f22dca1d25d60a8bedd4b
root <victor252260@gmail.com>
2023-08-13 21:02:59 -0700

    changes

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,54 +1,31 @@
 #include "lists.h"
+
 /**
- * is_palindrome - determine if a list is palindrome
- * @head: pointer to the first node
- * Return: 1 if is palindrome otherwise 0
+ * is_palindrome - function to call check_pal to see if list is palindrome
+ * @head: ptr to the beginning of the list
+ * Return: 0 if not palindrome else 1
  */
 int is_palindrome(listint_t **head)
 {
-	listint_t *temp = *head, *temp2 = NULL, *head2 = NULL;
-
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return (1);
-	while (temp != NULL)
-	{
-		new_f(&head2, temp->n);
-		temp = temp->next;
-	}
-	temp = *head;
-	temp2 = head2;
-	while (temp != NULL)
-	{
-		if (temp->n == temp2->n)
-		{
-			temp = temp->next;
-			temp2 = temp2->next;
-		}
-		else
-			break;
-	}
-	free_listint(head2);
-	if (temp == NULL)
-		return (1);
-	return (0);
+	return (check_pal(head, *head));
 }
-/**
- * new_f - add node to a linked list at the beginning
- * @head: address passed by reference
- * @n: value for the new node
- * Return: new node
- */
-listint_t *new_f(listint_t **head, int n)
-{
-	listint_t *new;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
+/**
+ * check_pal - function to check if the list is palindrome
+ * @head: ptr to the beginning of the list
+ * @last: ptr to the end of the list
+ * Return: 0 if not palindrom else 1
+ */
+int check_pal(listint_t **head, listint_t *last)
+{
+	if (last == NULL)
+		return (1);
+	if (check_pal(head, last->next) && (*head)->n == last->n)
 	{
-		return (NULL);
+		*head = (*head)->next;
+		return (1);
 	}
-	new->n = n;
-	new->next = *head;
-	*head = new;
-	return (new);
+	return (0);
 }
