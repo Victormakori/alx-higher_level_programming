9fa4cb8b9d7ea8010d27696011e00e9c2744589a
root <victor252260@gmail.com>
2023-08-30 05:45:10 -0700

    late changes

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
-class Square:
-    def __init__(self, size=0):
-        self.__size = size
 
-    def area(self):
-        return self.__size**2
+""" defining a square """
+
+
+class Square:
+    """ square representation """
+    def __init__(self, size=0):
+        self.size = size
 
     @property
     def size(self):
-        return self.__size
+        """ setting size """
+        return (self.__size)
 
     @size.setter
     def size(self, value):
-        self.__size = value
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if value < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ returning current area """
+        return (self.__size ** 2)
 
     def my_print(self):
+        """ printing square """
         if self.__size == 0:
-            print("")
-        else:
-            for x in range(self.__size):
-                for y in range(self.__size):
-                    print("#", end="")
-                print("")
+            print()
+            return
+        for _ in range(self.__size):
+            print("#" * self.__size)
