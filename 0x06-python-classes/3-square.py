b056be1107e85cac759d1c67097c285f33c86622
root <victor252260@gmail.com>
2023-08-30 05:39:38 -0700

    late changes

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
+
+""" a class Square """
+
+
 class Square:
+    """ Square representation """
     def __init__(self, size=0):
-        self.__size = size
+        if not isinstance(size, int):
+            raise TypeError("size must an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
+        self.__size = size
 
     def area(self):
-        return self.__size**2
+        return (self.__size ** 2)
