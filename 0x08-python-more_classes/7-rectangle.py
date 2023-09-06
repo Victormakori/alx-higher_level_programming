f2851c9ee413ab4a16ce77a8c2e9ae2a6d1b3a90
root <victor252260@gmail.com>
2023-09-05 17:52:41 -0700

    late changes

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,9 +1,20 @@
 #!/usr/bin/python3
-"""This module defines a Rectangle class."""
+"""Module 7-rectangle
+Defines a Rectangle class.
+"""
 
 
 class Rectangle:
-    """Rectangle class defined by width and height."""
+    """Rectangle class defined by width and height.
+
+    Attributes:
+        number_of_instances: number of Rectangle instances,
+        increments with every instantitation,
+        decrements with every deletion
+    """
+
+    number_of_instances = 0
+    print_symbol = '#'
 
     def __init__(self, width=0, height=0):
         """Initializes a Rectangle instance.
@@ -14,17 +25,17 @@ class Rectangle:
         """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     def __str__(self):
         """Returns an informal and nicely printable string representation
         of a Rectangle instance, filled with the '#' character."""
         if self.__height == 0 or self.__width == 0:
             return ''
-        """ Create a string representation of the rectangle"""
         rec_str = ''
         for i in range(self.__height):
             for j in range(self.__width):
-                rec_str += '#'
+                rec_str += str(self.print_symbol)
             rec_str += '\n'
         return rec_str[:-1]
 
@@ -37,6 +48,7 @@ class Rectangle:
     def __del__(self):
         """Deletes a Rectangle instance."""
         print("Bye rectangle...")
+        Rectangle.number_of_instances -= 1
 
     @property
     def width(self):
