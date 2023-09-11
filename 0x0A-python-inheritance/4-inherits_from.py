33a65d4149baebafc6ca5e9ff7f6432ef173adfa
root <victor252260@gmail.com>
2023-09-11 16:17:15 -0700

    late changes

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -3,5 +3,10 @@
 '''
 
 
-def inherits_from(obj, a_class);
+def inherits_from(obj, a_class):
+    '''the object is an instance of a class inherited (directly or indirectly)
+    obj: an object
+    a_class: a class
+    returns None
+    '''
     return type(obj) != a_class and isinstance(obj, a_class)
